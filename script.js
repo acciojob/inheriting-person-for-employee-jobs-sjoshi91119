d9997b82cd8d4fe23e81e0038bd1496ac749fc38
Shubham Joshi <115713904+sjoshi91119@users.noreply.github.com>
2023-04-18 13:16:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ function Person(name, age) {
 	greet(){
 	console.log(`Hello, my name is ${Person.name}, I am ${Person.age} years old.`);
 }
-}
+};
 
 function Employee(name, age, jobTitle) {
 	jobGreet(){
-	console.log(`Hello, my name is ${Employee.name, I am ${Employee.age} years old, and my job title is ${Employee.jobTitle}.`);
+	console.log(`Hello, my name is ${Employee.name}, I am ${Employee.age} years old, and my job title is ${Employee.jobTitle}.`);
 		}
 }
 
